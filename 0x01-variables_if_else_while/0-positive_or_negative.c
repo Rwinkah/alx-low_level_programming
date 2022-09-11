0151923c7efb316bd58f38791a1168531514aaab
Rwinkah <rwinosa@gmail.com>
2022-09-11 19:44:15 +0100

    fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,8 @@
-#include < stdlib.h >
+#include <stdlib.h>
 
-#include < time.h >
+#include <time.h>
 
-#include < stdio.h >
+#include <stdio.h>
 
 /*
  *main function picks a random number and checks
