792b4db4d286d8929d0835563a1838bd046297a7
Rwinkah <rwinosa@gmail.com>
2022-09-13 21:11:42 +0100

    4 task c variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Description - "alphabet program"
+*
+* Return: Always 0
+**/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+	if ( c == 'e' || c == 'q')
+{
+		continue;
+}
+	else
+{
+		putchar(c);
+}
+
+return (0);
+}
