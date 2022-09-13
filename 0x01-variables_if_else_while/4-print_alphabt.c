f165a95de526392b3fdf82174bd64a8245fa04f6
Rwinkah <rwinosa@gmail.com>
2022-09-13 21:21:18 +0100

    fix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-/**main - entry point*
-* Description - "alphabet program"
+/**
+* main - entry point*
+* Description : "alphabet program"
 *
 * Return: Always 0
 **/
@@ -8,7 +9,7 @@ int main(void)
 {
 char c;
 for (c = 'a'; c <= 'z'; ++c)
-	if ( c == 'e' || c == 'q')
+	if (c == 'e' || c == 'q')
 {
 		continue;
 }
