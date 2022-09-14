9761e682a549507ab8567bf4f0ccc056cb4bcde0
Rwinkah <rwinosa@gmail.com>
2022-09-14 16:27:21 +0100

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,9 @@
 * Description: "prints last digit of a number"
 *
 * @c: "The number to check"
-* 
+*
 * Return: "success = 0"
 **/
-
 int print_last_digit(int c)
 {
 if (c > 0)
@@ -20,6 +19,5 @@ else
 {
 c = c *-1;
 }
-_putchar('0' + c % 10);
 return (c % 10);
 }
