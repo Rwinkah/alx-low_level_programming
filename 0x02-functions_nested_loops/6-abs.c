896800bde3e6d4da8e676ff243e1a25628463b0f
Rwinkah <rwinosa@gmail.com>
2022-09-14 04:25:10 +0100

    6 task c functions/nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _abs - entry point
+*
+* Description: "check for lowercase letters"
+*
+* @c: "The letter to check"
+* Return: "success = 0"
+**/
+
+int _abs(int c) 
+{
+if (c > 0)
+return (c);
+else
+return (c * -1);
+{
