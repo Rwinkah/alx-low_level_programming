a13bc7f68a2c84a949e6a0fc4957d39a8f9847ab
Rwinkah <rwinosa@gmail.com>
2022-09-14 02:01:01 +0100

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
 #include "main.h"
 
-int main() {
+/**
+* main - entry poiny
+*
+* Description: "output string"
+*
+* Return: "success = 0"
+**/
 
-char x[9] ={95,112,117,116,99,104,97,114,10};
-int i =0;
+int main(void)
+{
+char x[9] = {95, 112, 117, 116, 99, 104, 97, 114, 10};
+int i = 0;
 for (; i <= 8; i++)
 	_putchar(x[i]);
-return(0);
+return (0);
 }
