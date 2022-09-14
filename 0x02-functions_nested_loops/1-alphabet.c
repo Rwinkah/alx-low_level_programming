fa6f83ff46f7bd4ae3014ee207110f9cf00462fb
Rwinkah <rwinosa@gmail.com>
2022-09-14 02:06:22 +0100

    1 tasks functions/loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - entry poiny
+*
+* Description: "output string"
+*
+* Return: "success = 0"
+**/
+
+int main(void)
+{
+int i = 97;
+for (; i <= 122; i++)
+	_putchar(i);
+_putchar(10);
+return (0);
+}
