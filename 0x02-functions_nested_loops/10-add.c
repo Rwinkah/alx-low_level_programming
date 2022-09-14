dedb6b5ac31f0bebba58f71d17ffb41410939b32
Rwinkah <rwinosa@gmail.com>
2022-09-14 05:19:58 +0100

    10 task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _add - entry point
+*
+* Description: "sum two numbers"
+*
+* @c: "first number"
+* @d: " second number"
+* Return: "sum"
+**/
+
+int add(int c, int d)
+{
+int sum = c + d;
+return (sum);
+}
