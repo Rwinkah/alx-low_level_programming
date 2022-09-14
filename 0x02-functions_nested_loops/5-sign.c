ffd2737130f5f3c8b450358799b564073e50af33
Rwinkah <rwinosa@gmail.com>
2022-09-14 03:53:23 +0100

    task 5 functions/nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_sign - entry point
+*
+* Description: "check for lowercase letters"
+*
+* @n: "The letter to check"
+* Return: "success = 0"
+**/
+
+int _print_sign(int n)
+{
+if (c > 0 )
+return (1);
+else
+if (c == 0)
+return (0);
+else
+return (-1);
+}
