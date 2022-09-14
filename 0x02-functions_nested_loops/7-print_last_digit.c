3b274ed69a9d51c8854a01fe8a302fc57ef7ef76
Rwinkah <rwinosa@gmail.com>
2022-09-14 04:33:10 +0100

    7 task functions/nested loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_last_digit - entry point
+*
+* Description: "prints lat digit of a number"
+*
+* @c: "The number to check"
+* Return: "success = 0"
+**/
+
+int print_last_digit(int c)
+{
+d = (c % 10);
+_putchar(d);
+return (0);
+}
