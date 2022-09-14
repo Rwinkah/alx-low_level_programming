bf2138908be8d8316a1f54891943a5bdac44bb0b
Rwinkah <rwinosa@gmail.com>
2022-09-14 12:32:37 +0100

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,11 +6,11 @@
 * Description: "prints lat digit of a number"
 *
 * @c: "The number to check"
+* 
 * Return: "success = 0"
 **/
 
 int print_last_digit(int c)
 {
-_putchar('0' + c%10);
-return (0);
+return (c % 10);
 }
