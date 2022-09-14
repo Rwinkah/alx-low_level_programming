f66f5ce54d8ac2ee85b269c5ff0a60023b0ed59a
Rwinkah <rwinosa@gmail.com>
2022-09-14 01:27:53 +0100

    0 task c functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - entry point
+*
+* Description: "print line"
+*
+* Return: 0
+**/
+
+int main(void)
+{
+char x[] = {95,112,117,116,99,104,97,114,10}
+for (int i =0; i <= 8; i++)
+	_putchar(x[i]);
+return (0);
+}
