0e976f85291b94770f8a3683de1ed60626457612
Rwinkah <rwinosa@gmail.com>
2022-09-14 20:18:58 +0100

    fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,42 +9,35 @@
 int main(void)
 {
 int c;
-
-    for (c = 0; c < 90; ++c)
+for (c = 0; c < 90; ++c)
 {
-    int d = c / 10;
-    int e = c % 10;
-    
-    if (d  == e)
-    {
-        continue;
-    }
-    else
-    {
-        if (d > e){
-            continue;
-        }
-        else {
-            putchar('0' + d);
-            putchar('0' + e);
-
-        }
-    }
-    
-    
-            
-        
-    if (c < 90)
+int d = c / 10;
+int e = c % 10;
+if (d  == e)
 {
-        putchar(44);
-        putchar(32);
+continue;
 }
-    else
-    {
-    break;
-    }
-
-
+else
+{
+if (d > e)
+{
+continue;
+}
+else
+{
+putchar('0' + d);
+putchar('0' + e);
+}
+}
+if (c < 89)
+{
+putchar(44);
+putchar(32);
+}
+else
+{
+break;
+}
 
 }
 putchar(10);
