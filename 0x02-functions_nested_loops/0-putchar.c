e3a7ecf88c78e58adcbb27a90dd0f411176b7185
Rwinkah <rwinosa@gmail.com>
2022-09-14 02:14:38 +0100

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,11 +8,12 @@
 * Return: "success = 0"
 **/
 
-int main(void)
+int print_alphabet(void)
 {
 char x[9] = {95, 112, 117, 116, 99, 104, 97, 114, 10};
 int i = 0;
 for (; i <= 8; i++)
 	_putchar(x[i]);
 return (0);
+
 }
