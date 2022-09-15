1a301402281090c91e30f55903e6a1a32fe858c6
Rwinkah <rwinosa@gmail.com>
2022-09-15 23:29:04 +0100

    5 task more functions in c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* more_numbers - entry point
+*
+* Return: none
+**/
+void more_numbers(void)
+{
+int c;
+int i;
+
+for (i = 0; i <= 10; i++)
+{
+for (c = 0; c <= 14; ++c)
+{
+_putchar('0' + c);
+}
+_putchar(10);
+}
+_putchar(10);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,3 +10,4 @@ void times_table(void);
 void print_times_table(int n);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
