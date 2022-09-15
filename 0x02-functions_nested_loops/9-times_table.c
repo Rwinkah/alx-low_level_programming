8325863b444ede32ace46364007cf46e806a2871
Rwinkah <rwinosa@gmail.com>
2022-09-15 14:32:30 +0100

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,20 +16,26 @@ for (i = 0; i < 10; i++)
 int k;
 for (k = 0; k < 10; k++)
 {
-int d = i * k;
-int e = d / 10;
-int f = d % 10;
+int d = i * k;, int e = d / 10;, int f = d % 10;
 if (d < 10)
 {
 _putchar('0' + f);
-if (d < 9)
+if (k == 9)
+continue;
+if (((k + 1) * i) <= 9)
 {
 _putchar(44);
-_putchar(32);
-_putchar(32);
+_putchar(46);
+_putchar(46);
 }
 else
 {
+if (((k + 1) * i) > 9)
+{
+_putchar(44);
+_putchar(46);
+}
+else
 continue;
 }
 }
@@ -37,16 +43,15 @@ else
 {
 _putchar('0' + e);
 _putchar('0' + f);
-if (d < 9)
+if (k < 9)
 {
 _putchar(44);
-_putchar(32);
+_putchar(46);
 }
 else
-{
 continue;
 }
 }
-_putchar(10);
+_putchar(10);             
 }
-
+}
