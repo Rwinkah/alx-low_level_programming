e1f21ad96cc8aea2dd9769304055c264089d2989
Rwinkah <rwinosa@gmail.com>
2022-09-15 23:36:02 +0100

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,7 +14,15 @@ for (i = 0; i < 10; i++)
 {
 for (c = 0; c <= 14; ++c)
 {
+if (c <= 9)
+{
 _putchar('0' + c);
+}
+else 
+{
+_putchar('0' + (c / 10));
+_putchar('0' + (c % 10));
+}
 }
 _putchar(10);
 }
