5ad6dafae90f4d196cc1501886dc52f98721f4dd
Rwinkah <rwinosa@gmail.com>
2022-09-15 23:37:37 +0100

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,7 +9,6 @@ void more_numbers(void)
 {
 int c;
 int i;
-
 for (i = 0; i < 10; i++)
 {
 for (c = 0; c <= 14; ++c)
@@ -18,7 +17,7 @@ if (c <= 9)
 {
 _putchar('0' + c);
 }
-else 
+else
 {
 _putchar('0' + (c / 10));
 _putchar('0' + (c % 10));
