cc09b962173b294136c383b7b93b1060afb8a7e8
Rwinkah <rwinosa@gmail.com>
2022-09-15 22:57:47 +0100

    4 task more functions in c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_most_numbers - entry point
+*
+* Description - "print numbers 0-9 except 2 and 4"
+*
+* Return: none
+**/
+void print_most_numbers(void)
+{
+int c;
+
+for (c = '0'; c <= '9'; ++c)
+if (c ==2 || c ==4)
+{
+continue;
+}
+else
+{
+	_putchar(c);
+}
+_putchar('\n');
+
+}
