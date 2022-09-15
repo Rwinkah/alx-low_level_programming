ce449f76fd491f5bb6471981a9001e90249ca11c
Rwinkah <rwinosa@gmail.com>
2022-09-15 23:22:35 +0100

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,15 +3,12 @@
 /**
 * print_most_numbers - entry point
 *
-* Description: "print number 0-9 except 2 and 4"
-*
 * Return: none
 **/
-
 void print_most_numbers(void)
 {
 int c;
-for (c = '0'; c <= '9'; ++c)
+for (c = 0; c <= 9; ++c)
 {
 if (c == 2 || c == 4)
 {
@@ -19,7 +16,7 @@ continue;
 }
 else
 {
-_putchar(c);
+_putchar('0' + c);
 }
 }
 _putchar(10);
