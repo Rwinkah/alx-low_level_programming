932eaf35846762efb27c71b3bfd40bece2af1ac4
Rwinkah <rwinosa@gmail.com>
2022-09-16 04:51:56 +0100

    8 task more functions c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+* print_square - entry point
+*@n: input number
+* Description: "print a line"
+* Return: none
+**/
+
+void print_square(int n)
+{
+int i;
+
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+int k;
+for (k = 0; k < n; k++)
+{
+_putchar(35);
+}
+_putchar(10);
+}
+
+
+
+
+
+
+}
+else
+{
+_putchar(10);
+}
+}
