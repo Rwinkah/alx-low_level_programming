a77e2ffba1586f7969eeb672bd9fd4d4d389376d
Rwinkah <rwinosa@gmail.com>
2022-09-16 04:19:09 +0100

    6 task more functions c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_line - entry point
+*@n: input number
+* Description: "print a line"
+* Return: none
+**/
+
+void print_line(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 0; i != n; i++)
+{
+_putchar(95);
+}
+_putchar(10);
+}
+
+else
+{
+_putchar(10);
+}
+}
