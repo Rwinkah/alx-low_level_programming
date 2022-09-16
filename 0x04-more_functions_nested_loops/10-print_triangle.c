619a67c7897fb7516db176750f912033c54b8ae1
Rwinkah <rwinosa@gmail.com>
2022-09-16 06:42:01 +0100

    10 task more functions c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,62 @@
+#include "main.h"
+void space(int i, int n);
+/**
+* print_triangle - entry point
+*
+*@n: input number
+*
+* Description: "print triangle"
+*
+* Return: none
+**/
+
+
+
+void print_triangle(int n)
+{
+if (n <= 0)
+{
+_putchar(10);
+}
+else
+{
+int i;
+int k;
+
+for (i = 0; i <= n; i++)
+{
+space(i, n);
+for (k = 0; k < i; k++)
+{
+_putchar(35);
+}
+_putchar(10);
+}
+}
+}
+
+
+/**
+* space - design
+*
+*@n: input number
+*
+*@i: count number
+* Description: "print space"
+*
+* Return: none
+**/
+
+
+
+void space(int i, int n)
+{
+if (i != 10)
+{
+int k;
+for (k = 0; k < (n - i); k++)
+{
+_putchar(32);
+}
+}
+}
