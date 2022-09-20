aca5a90a92b9de9829b7dd6bdac41b2b7309b720
Rwinkah <rwinosa@gmail.com>
2022-09-20 05:33:19 +0100

    task array/pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,7 +3,7 @@
 /**
 *puts2 - print a string in alternation
 *
-* @s: string pointer to be used
+* @str: string pointer to be used
 *
 * Return: void
 **/
@@ -11,15 +11,15 @@
 void puts2(char *str)
 {
 int j = 0;
-while (*s != '\0')
+while (*str != '\0')
 {
 s++;
 j++;
 j++;
-_putchar(*s);
+_putchar(*str);
 }
 
-}
+
 
 _putchar(10);
 }
