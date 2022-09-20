13fb61055f339158507e1f5c5c148548f9e403df
Rwinkah <rwinosa@gmail.com>
2022-09-20 01:22:44 +0100

    task 4 arrays/pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_rev - print a string in reverse
+*
+* @s: string pointer to be used
+*
+* Return: void
+**/
+
+void print_rev(char *s)
+{
+int j = 0;
+while (*str != '\0')
+{
+str++;
+j++
+}
+
+while (j > 0)
+{
+_putchar(*s);
+s--;
+j--
+}
+
+_putchar(10);
+}
