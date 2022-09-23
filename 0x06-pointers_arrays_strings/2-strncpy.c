001058bac6b71d1611b3a3fe84063292bc31a17c
Rwinkah <rwinosa@gmail.com>
2022-09-23 14:09:07 +0100

    fix betty

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*_strncpy - copies n bytes of str to dest  
+*_strncpy - copies n bytes of str to dest
 *
 *@dest: pointer to destination array
 *@src: pointer to source array
@@ -14,7 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int j;
 
-for(j = 0; j < n; j++)
+for (j = 0; j < n; j++)
 {
 dest[j] = src[j];
 }
