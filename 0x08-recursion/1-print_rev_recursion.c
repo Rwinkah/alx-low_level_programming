0f55f0be598170fedf0ca3df2ac39b47d8367920
Rwinkah <rwinosa@gmail.com>
2022-09-29 00:48:15 +0100

    fix betty

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
-/** 
+/**
 * _print_rev_recursion - print a string in reverse
-* 
-* @s: start of the string 
+*
+* @s: start of the string
 *
 * Return: void
 **/
 
 
-void _print_rev_recursion( char *s)
+void _print_rev_recursion(char *s)
 {
 
 if (*s != '\0')
