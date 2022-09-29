d2675326ae4b8525c207d882f45748d79395be67
Rwinkah <rwinosa@gmail.com>
2022-09-29 20:27:31 +0100

    fix betty

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,35 +1,35 @@
 
 /**
-* prme - check for prime numbers
-*  
-* @n: number to check 
-* @x: count  
+* prm - check for prime numbers
+*
+* @n: number to check
+* @x: count
 * Return: 1 if prime 0 if not prime
 **/
 
-int prm(int n, int x) 
+int prm(int n, int x)
 {
 if ((n == 3) || (n == 2))
 return (1);
 
 if (n <= 1)
-return (0); 
-
-if (((n % x) == 0) && ( x != 1))
 return (0);
 
-if ((x > (n/2)))
-return(1);
+if (((n % x) == 0) && (x != 1))
+return (0);
 
-else 
+if ((x > (n / 2)))
+return (1);
+
+else
 return (prm(n, x + 1));
 }
 
 /**
 * is_prime_number - check for prime numbers
-*  
-* @n: number to check 
-* 
+*
+* @n: number to check
+*
 * Return: 1 if prime 0 if not prime
 **/
 
