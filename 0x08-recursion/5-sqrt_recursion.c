787afa533f866127a0243da5bacf1951340e0b1d
Rwinkah <rwinosa@gmail.com>
2022-09-29 21:38:06 +0100

    task 5 0x08

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+
+/**
+* sqr - check for prime numbers
+*
+* @n: number to check
+* @x: count
+* Return: int
+**/
+
+int sqr(int n, int x)
+{
+
+if (x * x == n)
+return (x);
+
+if ((x > (n / 2)) && (x * x != n))
+return (-1);
+
+else
+return (sqr(n, (x + 1)));
+}
+
+/**
+* sqrt_recursion- find square root
+*
+* @n: number to check
+*
+* Return: int
+**/
+
+int _sqrt_recursion(int n)
+{
+return (sqr(n, 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -44,3 +44,4 @@ int factorial(int n);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int _sqrt_recursion(int n);
