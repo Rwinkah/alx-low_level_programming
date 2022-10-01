8c5139debc463ac73ab987c1658af0da0a92005a
Rwinkah <rwinosa@gmail.com>
2022-10-01 03:03:19 +0100

    fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,12 +12,13 @@
 
 int main(int argc, char *argv[])
 {
+int mul; 
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-int mul = atoi(argv[1]) * atoi(argv[2]);
+mul = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n", mul);
 return (0);
 }
