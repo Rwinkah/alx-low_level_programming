b2d7cdf133f467d8ba9b23ee74b6a7772ec993fe
Rwinkah <rwinosa@gmail.com>
2022-10-05 00:20:51 +0100

    fix handle null value

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,12 +13,20 @@ char *str_concat(char *s1, char *s2)
 {
 int i, j, k, l, m;
 char *p;
+if(s1 == NULL)
+s1 = "";
+
+if (s2 == NULL)
+s2 = "";
+
 for (i = 0; s1[i] != '\0'; i++)
 ;
 for (j = 0; s2[j] != '\0'; j++)
 ;
 l = j + 1 + i;
 p = malloc(l *sizeof(char));
+if (p == NULL)
+return (0);
 
 for (k = 0; k < i; k++)
 {
