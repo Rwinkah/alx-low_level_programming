3b344ab0129e08d24109a90b4bc7ea949e459f7f
Rwinkah <rwinosa@gmail.com>
2022-10-04 22:33:46 +0100

    fix betty and null input

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,12 @@ char *create_array(unsigned int size, char c)
 {
 char *p;
 unsigned int i;
-p =(char *)malloc(sizeof(char) * size);
+
+if (size == 0)
+{
+return ('\0');
+}
+p = (char *)malloc(sizeof(char) * size);
 
 for (i = 0; i < size; i++)
 {
