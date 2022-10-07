cc5ae5a3ce72b4ae4d94717d0ffa0c6db0605cb5
Rwinkah <rwinosa@gmail.com>
2022-10-07 01:04:35 +0100

    task 3 0x0C

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+/**
+* _calloc - implementation of calloc
+*
+* @nmemb: number of units
+* @size: size of each unit
+*
+* Return: void pointer
+**/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *p;
+char *mem;
+unsigned int i;
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+p = malloc(nmemb * size);
+if (p == 0)
+{
+return (NULL);
+}
+
+mem = p;
+for (i = 0; i < sizeof(p); i++)
+{
+mem[i] = 0;
+}
+
+return (mem);
+}
+
+
