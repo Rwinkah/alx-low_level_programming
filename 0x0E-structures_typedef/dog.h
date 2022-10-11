d3dd026c6d7e0b0b56b6088acba48f52f0bbb479
Rwinkah <rwinosa@gmail.com>
2022-10-11 21:04:11 +0100

    fix:betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,19 @@
-#pragma once
-struct dog {
+#ifndef __DOG__
+#define __DOG__
+
+/**
+* struct dog - dog structure
+* @name: name of dog
+* @age: age of the dog
+* @owner: owner of the dog
+*
+*
+**/
+
+struct dog
+{
 char *name;
 float age;
 char *owner;
 };
+#endif
