e5982ef911a8bfafc2061aca869e923b716d1774
Rwinkah <rwinosa@gmail.com>
2022-10-12 00:11:24 +0100

    fix: everything

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,30 +10,75 @@
 * Return: dog_t 
 **/ 
 
+
+
+
+
+
+/**
+ * _strlen - find the length of a string
+ * @s: pointer to string
+ *
+ * Return: int 
+ **/
+int _strlen(char *s)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+;
+return (i);
+}
+
+/**
+ * _strcpy - copy from srt to dest
+ * @dest: destination
+ * @src:  source
+ *
+ * Return: dest 
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+
+dest[i] = '\0';
+return (dest);
+}
 dog_t *new_dog(char *name, float age, char *owner)
 {
+dog_t *new_dog;
+int lname, lowner;
 
+new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+return (NULL);
 
-char *sowner = owner;
-char *sname = name;
-
-dog_t *d;
-
-*d = malloc(sizeof dog_t);
-if (d == NULL)
+lname = _strlen(name);
+new_dog->name = malloc(sizeof(char) * lname + 1);
+if (new_dog->name == NULL)
 {
+free(new_dog);
+return (NULL);
+}
+new_dog->name = _strcpy(new_dog->name, name);
+lowner = _strlen(owner);
+new_dog->owner = malloc(sizeof(char) * lowner + 1);
+if (new_dog->owner == NULL)
+{
+free(new_dog->name);
+free(new_dog);
 return (NULL);
 }
 
+new_dog->owner = _strcpy(new_dog->owner, owner);
+new_dog->age = age;
 
-d->name = malloc(20);
-
-
-d->owner = malloc(20);
-
-
-d->name = sname;
-d->age = age;
-d->owner = sowner;
-return (d);
+return (new_dog);
 }
+
