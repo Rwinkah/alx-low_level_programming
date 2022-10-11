613fd4406dd71ec39a3f20d8de04e76c9556bf1d
Rwinkah <rwinosa@gmail.com>
2022-10-11 21:17:23 +0100

    task 1 0x0E

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+* init_dog - bring the dog to life
+*
+* @d: name of the struct
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*
+* Return: void
+**/
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
