2b821bda115abb118c22fcb33c56a64c18d19787
Rwinkah <rwinosa@gmail.com>
2022-10-11 22:57:30 +0100

    fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,11 +20,8 @@ printf("Name: %s\n", d->name);
 else
 printf("Name: (nil)\n");
 
-if (d->age != 0)
 printf("Age: %f\n", d->age);
 
-else
-printf("Age: (nil)\n");
 
 if (d->owner != NULL)
 printf("Owner: %s\n", d->owner);
@@ -33,5 +30,3 @@ else
 printf("Owner: (nil)\n");
 
 }
-
-
