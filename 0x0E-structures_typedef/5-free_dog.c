8cf549ab17f4f50e4a4b0c7c79301c0c7ea3fb6c
Rwinkah <rwinosa@gmail.com>
2022-10-12 00:57:57 +0100

    task 5 0x0E

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+/**
+ * free_dog - free dog struct
+ * @d: pointer to the dog
+ *
+ * Return: void
+ **/
+
+
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
