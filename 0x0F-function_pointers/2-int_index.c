b48cfe141f50b38820f86af57c0221e61a0a371d
Rwinkah <rwinosa@gmail.com>
2022-10-13 00:45:20 +0100

    task 2 0x0F

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * int_index - find an int
+ *
+ * @array: array to search
+ * @size: size of the array
+ * @cmp: compare function
+ *
+ * Return: int
+ **/
+
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+if (cmp != NULL)
+{
+int i = 0;
+
+if (size <= 0)
+{
+return (-1);
+}
+
+while (i < size)
+{
+if (cmp(array[i]) != 0)
+{
+return (i);
+}
+else
+i++;
+}
+}
+
+return (-1);
+}
