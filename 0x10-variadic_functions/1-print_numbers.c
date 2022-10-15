a63398ac3355c9de18b7906e2d8633276c24cc3c
Rwinkah <rwinosa@gmail.com>
2022-10-15 02:00:05 +0100

    task 1 0x10

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,37 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers in a list
+ *
+ * @n: number of arguments
+ * @seperator: seperator for numbers
+ *
+ * Return: int
+**/
+
+
+void print_numbers(const char *seperator, const unsigned int n, ...)
+{
+va_list ap;
+unsigned int i;
+va_start(ap, n);
+if (seperator != NULL)
+{
+
+for (i = 0; i < n; i++)
+{
+printf("%d%s", va_arg(ap, int), seperator);
+}
+}
+
+else
+{
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(ap, int));
+}
+putchar(10);
+va_end(ap);
+}
+}
