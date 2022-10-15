17d8f3faa3b42c807aff21bc050e59345fba474f
Rwinkah <rwinosa@gmail.com>
2022-10-15 02:31:36 +0100

    fix seperator

--- 0x10-variadic_functions/1-main.c
+++ 0x10-variadic_functions/1-main.c
@@ -11,6 +11,6 @@ int main(void)
     print_numbers(", ", 4, 0, 98, -1024, 402);
     print_numbers("; ", 5, 30, 938, -123024, 43402, 0);
     print_numbers("; ", 0);
-
+    print_numbers("; ", 1, 30);
     return (0);
 }
