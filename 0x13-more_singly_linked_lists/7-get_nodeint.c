c4b52d2ecf87a795ebb82eec103c0a459d95b22c
Rwinkah <rwinosa@gmail.com>
2022-10-26 04:44:45 +0100

    fix: null check

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,6 +15,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 listint_t *ptr = head;
 unsigned int i = 0;
 
+
+
+
+if (!head)
+{
+return (NULL);
+}
+
 while (i < index)
 {
 ptr = ptr->next;
