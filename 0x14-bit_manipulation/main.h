bf5a991af14a7c897d425383d53c8ffd8dd89406
Rwinkah <rwinosa@gmail.com>
2022-10-29 01:04:36 +0100

    0 task 0x14

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,6 @@
+#ifndef __MAINN__
+#defined __MAIN__
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
---
+++ 0x14-bit_manipulation/task-0.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+
+/**
+ * binary_to_uint - convert binary to uint
+ *
+ * @b: start of string representation of binary
+ *
+ * Return: unsigned int
+ **/
+
+unsigned int binary_to_uint(const char *b)
+{
+int strlen;
+int i = 0, sum = 0;
+int value = 1;
+for (strlen = 0; b[strlen] != '\0'; strlen++)
+;
+i = strlen - 1;
+for (; i >= 0; i--)
+{
+if (b[i] != '0' || b[i] != '1')
+{
+return (0);
+}
+if (b[i] == '1')
+{
+sum += value;
+}
+value *= 2;
+}
+return (sum);;
+} 
