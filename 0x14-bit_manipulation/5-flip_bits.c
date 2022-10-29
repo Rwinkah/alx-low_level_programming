a6b0dac710ae7f45e086c61596d545ec4e125645
Rwinkah <rwinosa@gmail.com>
2022-10-29 02:00:02 +0100

    5 task 0x14

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * flip_bits - number of bits  to flip to get from initial num to required
+ * @n: initial num
+ * @m: destination
+ *
+ * Return: unsigned int
+ **/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i, flips = 0;
+	unsigned long int j = sizeof(unsigned long int) * 8;
+
+	for (i = 0; i < j; i++)
+	{
+		if ((m & 1) != (n & 1))
+			flips += 1;
+		n = n >> 1;
+		m = m >> 1;
+	}
+	return (flips);
+}
