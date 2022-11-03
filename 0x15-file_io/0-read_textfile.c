2a3fa0df1f58bbb56e7c65e8e65c661d62829138
Rwinkah <rwinosa@gmail.com>
2022-11-04 00:58:10 +0100

    fix: return wrong value

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,31 +15,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
 	size_t ans;
-	int test;
+	int test_read;
 	int fd = open(filename, O_CREAT | O_RDONLY);
-	if (!filename)
-	{
-		return (0);
-	}
 	buf = malloc(letters);
 
-	if (fd < 0 || !buf)
-	{
-		return (-1);
-	}
-	test = read(fd, buf, letters);
-	if (test == -1)
-	{
-		return (-1);
-	}
-
-	buf[letters] = '\0';
-
-	ans = write(STDOUT_FILENO, buf, letters);
-	if (ans != letters)
+	if (!filename || !buf)
 	{
 		return (0);
 	}
-
-	return (ans);
+	test_read = read(fd, buf, letters);
+	if (test_read == -1)
+	{
+		return (-1);
+	}
+	buf[letters] = '\0';
+	ans = write(STDOUT_FILENO, buf, letters);
+	if (!ans)
+	{
+		return (0);
+	}
+	return (test_read);
 }
