4401e26f4e45f56f5133657a01bba591658c55e2
Rwinkah <rwinosa@gmail.com>
2022-11-04 02:17:00 +0100

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	test_read = read(fd, buf, letters);
-	if (test_read < 0)
+	if (test_read <= 0)
 	{
 		return (0);
 	}
