4e41067fa01c5cda1946e1b88caccbd51cbdaf46
Rwinkah <rwinosa@gmail.com>
2023-01-23 14:01:41 +0100

    5 task 0x17

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free all nodes in a doubly linked list
+ *
+ * @head: head of the list
+ *
+ * Return: void
+ **/
+
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *tmp;
+
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+
+	while ((tmp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
+}
+
+free(tmp);
+}
---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get node at index
+ *
+ * @head: begining of the list
+ * @index: index of the element
+ *
+ * Return: dlistint_t
+ **/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp = head;
+	unsigned int n = 0;
+
+	if (!temp)
+	{
+		return (NULL);
+	}
+	for (n = 0; n < index; n++)
+		temp = temp->next;
+	return (temp);
+}
