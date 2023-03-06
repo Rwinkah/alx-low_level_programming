10babbcc07034aa759eec53497c07511b4a4aa37
Rwinkah <rwinosa@gmail.com>
2023-03-06 21:41:43 +0100

    fix

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -6,18 +6,10 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *freememory;
 
-	if (head == NULL)
-		return;
-
-
 	while (head != NULL)
 	{
 		freememory = head->next;
 		free(head);
 		head = freememory;
 	}
-	if (head)
-		free(head);
-	if (freememory)	
-		free(freememory);
 }
