13feaf41977176b75ab3d64e00ee12e7ee7ec6c8
Rwinkah <rwinosa@gmail.com>
2023-04-04 21:55:34 +0100

    0 task 0x1A

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_t - Create a hash table
+ *
+ * @size: Size of the hash table
+ *
+ * Return: hash_table_t pointer
+ **/
+
+hash_table_t *hash_table_create(unsigned long int size)
+    {
+        hash_table_t *h_table = (hash_table_t*)malloc(sizeof(hash_table_t));
+        h_table->size = size;
+        h_table->array = (hash_node_t**) malloc(sizeof(hash_node_t*) * size);
+        return(h_table);
+    } 
