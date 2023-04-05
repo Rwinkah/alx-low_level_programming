e73de9fdf89e2ef167a4ce22c2c23059b30472ff
Rwinkah <rwinosa@gmail.com>
2023-04-05 02:44:03 +0100

    fix: linter, null error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *h_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
+	if (!h_table)
+		return (NULL);
 	h_table->size = size;
 	h_table->array = (hash_node_t **) malloc(sizeof(hash_node_t *) * size);
 	return (h_table);
