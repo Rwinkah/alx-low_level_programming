133aaf3e74209a892d4ee25d8a74edd3e8d38ab2
Rwinkah <rwinosa@gmail.com>
2023-04-05 18:29:53 +0100

    fix

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,7 +9,7 @@
  * Return: index
  **/
 
-unsigned long int key_index (const unsigned char *key, unsigned long int size)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int value = hash_djb2(key) % size;
 
