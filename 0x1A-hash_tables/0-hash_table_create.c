8aa7604e575f9ec354760abd5349479285bce990
Rwinkah <rwinosa@gmail.com>
2023-04-05 02:38:49 +0100

    fix: linter

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,9 +9,10 @@
  **/
 
 hash_table_t *hash_table_create(unsigned long int size)
-    {
-        hash_table_t *h_table = (hash_table_t*)malloc(sizeof(hash_table_t));
-        h_table->size = size;
-        h_table->array = (hash_node_t**) malloc(sizeof(hash_node_t*) * size);
-        return(h_table);
-    } 
+{
+	hash_table_t *h_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+
+	h_table->size = size;
+	h_table->array = (hash_node_t **) malloc(sizeof(hash_node_t *) * size);
+	return (h_table);
+}
