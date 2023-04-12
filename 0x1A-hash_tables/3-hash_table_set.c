bae9114a932e99d6709f03a781fa09cedff099e2
Rwinkah <rwinosa@gmail.com>
2023-04-12 02:25:47 +0100

    fix: type

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if ((!new_node) || *key == '\0' || (!ht) || (!value) || (!key))
 		return (0);
-	index = key_index(key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
