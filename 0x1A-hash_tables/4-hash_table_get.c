ad6836ef287020d421ec07ba537c34ef6f6a1ae8
Rwinkah <rwinosa@gmail.com>
2023-04-12 03:26:06 +0100

    4 task 0x1A

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get value associated with a key
+ *
+ * @ht: the hashtable
+ *
+ * @key: the key to search with
+ *
+ * Return: char *
+ **/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *temp;
+	unsigned long int index;
+
+	if (!ht || !key || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	temp = ht->array[index];
+	while (temp && strcmp(temp->key, key) != 0)
+		temp = temp->next;
+
+	return ((temp == NULL) ? NULL : temp->value);
+}
