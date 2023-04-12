44474a0e8391edaae3a54e4be4f59d038cccba0d
Rwinkah <rwinosa@gmail.com>
2023-04-12 01:34:34 +0100

    3 task 0x1A

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to a hash table
+ *
+ * @ht: hash table
+ * @key: key of the item
+ * @value: value associated with the key
+ *
+ * Return: integer
+ **/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	unsigned long int index;
+	hash_node_t *temp;
+
+	if ((!new_node) || *key == '\0' || (!ht) || (!value) || (!key))
+		return (0);
+	index = key_index(key, ht->size);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	temp = ht->array[index];
+	if (!temp)
+	{
+		temp = new_node;
+	}
+
+	else
+	{
+		while (temp)
+		{
+			temp = temp->next;
+		}
+		temp = new_node;
+	}
+	return (1);
+}
