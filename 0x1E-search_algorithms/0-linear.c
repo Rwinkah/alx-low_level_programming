70dcf4bbd47e03accdf88afb00d8f86a31ccdfc1
rwinkah <rwinosa@gmail.com>
2023-07-16 04:25:08 +0100

    0 task 0x0E

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - c program using linear search algorithmm on a list
+ *
+ * @array: Head of the array
+ *
+ * @size: Size of the array
+ *
+ * @value: value to be found
+ *
+ * Return: int
+ **/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (!array)
+		return -1;
+	while (i < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (value == array[i])
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (-1);
+}
