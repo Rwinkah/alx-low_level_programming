b03a8aaa4cf0cdcfda5b7e29f18de7367c8ab91d
rwinkah <rwinosa@gmail.com>
2023-07-16 04:42:35 +0100

    1 task 0x0E

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,7 +17,7 @@ int linear_search(int *array, size_t size, int value)
 	size_t i = 0;
 
 	if (!array)
-		return -1;
+		return (-1);
 	while (i < size)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,45 @@
+#include "search_algos.c"
+
+/**
+ * binary_search - c implementation of binary search algorithm
+ *
+ * @array: Head of the array
+ *
+ * @size: size of the array
+ *
+ * @value: value to find.
+ *
+ * Return: int
+ **/
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left = 0, right = size - 1, middle = 0, i = 0;
+
+	if (array)
+	{
+		while (left <= right)
+		{
+			middle = (left + right) / 2;
+			printf("Searching in array: ");
+			for (i = left; i < right; i++)
+			{
+				printf("%d, ", array[i]);
+			}
+			printf("%d\n", array[i]);
+			if (array[middle] < value)
+			{
+				left = middle + 1;
+			}
+			else if (array[middle] > value)
+			{
+				right = middle - 1;
+			}
+			else
+			{
+				return (middle);
+			}
+		}
+	}
+	return (-1);
+}
